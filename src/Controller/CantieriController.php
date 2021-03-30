96c7f1a269de068d6e14b7470ce7247272e9390c
FrancoBagnoli <info@masotech.it>
2021-03-30 22:59:48 +0200

    Inizio modifiche per controller feedback cantieri

--- src/Controller/CantieriController.php
+++ src/Controller/CantieriController.php
@@ -2,29 +2,62 @@
 
 namespace App\Controller;
 
+use App\Entity\Cantieri;
+use App\Repository\CommentiPubbliciRepository;
+use App\Repository\CantieriRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class CantieriController extends AbstractController
-{
+{   
+    private $twig;
+
+    public function __construct(Environment $twig)
+    {
+    $this->twig = $twig; 
+    }
+
+
     /**
      * @Route("/", name="homepage")
      */
-    public function index(): Response
+    public function index( CantieriRepository $cantieriRepository): Response
     {
-         return new Response(<<<EOF
+        
+        return new Response($this->twig->render('cantieri/index.html.twig', [
+            'cantieri' => $cantieriRepository->findBy(['isPublic' => true], ['createdAt' => 'DESC']),
+            //  'cantieri' => $cantieriRepository->findAll(),
+        ]));
+
+        /* return new Response(<<<EOF  
         <html>
             <body>
                 <h1>Cantieri al lavoro</h1>
                 <img src="/images/under-construction.gif" />
             </body>
         </html>
-        EOF
-                );      
+        EOF  );       */
     }
 
-    
 
+    /**
+     * @Route("/cantieri/{id}", name="cantieri")
+     */
+     public function show(Request $request, Cantieri $cantieri, CommentiPubbliciRepository $commentiPubbliciRepository): Response
+    {
+        $offset = max(0, $request->query->getInt('offset', 0));
+        $paginator = $commentiPubbliciRepository->getCommentPaginator($cantieri, $offset);
+        
+
+        return new Response($this->twig->render('cantieri/show.html.twig', [
+            'cantieri' => $cantieri,
+            'commenti' => $paginator,
+            'previous' => $offset - $commentiPubbliciRepository::PAGINATOR_PER_PAGE,
+            'next' => min(count($paginator), $offset + $commentiPubbliciRepository::PAGINATOR_PER_PAGE),
+             ])); 
+        }   
 
 }
--- src/Repository/CommentiPubbliciRepository.php
+++ src/Repository/CommentiPubbliciRepository.php
@@ -3,8 +3,10 @@
 namespace App\Repository;
 
 use App\Entity\CommentiPubblici;
+use App\Entity\Cantieri;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 
 /**
  * @method CommentiPubblici|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,9 +16,27 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class CommentiPubbliciRepository extends ServiceEntityRepository
 {
+    public const PAGINATOR_PER_PAGE = 2;
+
     public function __construct(ManagerRegistry $registry)
     {
+    
         parent::__construct($registry, CommentiPubblici::class);
+    }
+
+    public function getCommentPaginator(Cantieri $cantieri, int $offset): Paginator
+    {
+            $query = $this->createQueryBuilder('c')
+           ->andWhere('c.cantieri = :cantieri')
+            ->setParameter('cantieri', $cantieri)
+            ->orderBy('c.createdAt', 'DESC')
+            ->setMaxResults(self::PAGINATOR_PER_PAGE)
+            ->setFirstResult($offset)
+            ->getQuery()
+            ;
+            
+            return new Paginator($query); 
+        
     }
 
     // /**
